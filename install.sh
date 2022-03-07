0bb866344a4a2f88e31612e7e785c2ed877fdbe5
Levi V <levi@redhat.com>
2022-03-07 14:37:37 +0100

    update to requirements and install script

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ path_to_script="$(realpath $(dirname "$0"))"
 enki=$path_to_script'/src/enki.py'
 
 # install dependencies
-pip install -r $path_to_script'/requirements.txt'
+pip install -qr $path_to_script'/requirements.txt'
 
 if [ "grep enki.py ~/.bashrc" ]; then
     sed -i "\|enki.py|d" ~/.bashrc
