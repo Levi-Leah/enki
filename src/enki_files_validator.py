ec7025d4733196c849f5c93f9c85f842c34732c6
Levi V <levi@redhat.com>
2022-09-05 13:23:06 +0200

    trying to resolve type errors 2

--- src/enki_files_validator.py
+++ src/enki_files_validator.py
@@ -93,10 +93,5 @@ def validating_files(files: list[str], start_time: float, output: str = None) ->
     if file_validation.count == 0:
         sys.exit(0)
 
-    if output == 'gitlab' or 'oneline':
-
-        file_validation.print_report(start_time, output)
-        sys.exit(2)
-
-    file_validation.print_report(output)
+    file_validation.print_report(start_time, output)
     sys.exit(2)
