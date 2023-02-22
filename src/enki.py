0d98ee844186ff68438d785fbf1e87487f097e05
Levi V <levi@redhat.com>
2023-02-22 17:04:41 +0100

    mods argspace; removes validate subcommand

--- src/enki.py
+++ src/enki.py
@@ -7,7 +7,7 @@ import sys
 import time
 import logging
 
-from enki_files_validator import validating_files
+from enki_files_validator import validating_files, lcheck_validate
 
 
 def main() -> None:
@@ -24,46 +24,62 @@ def main() -> None:
 
     user_input = args.path
 
-    if args.command == 'validate':
-        validate(user_input, args)
+    # if args.command == 'validate':
+    #     validate(user_input, args)
+    adoc_files, unsupported_files = collect_files(user_input)
+
+    if unsupported_files:
+        separator = "\n\t"
+        logging.error(f'Not adoc file. The following files cannot be validated:\n\t{separator.join(unsupported_files)}\n')
+        sys.exit(2)
+
+    if adoc_files:
+        start = time.time()
+
+        if args.validate:
+            validating_files(adoc_files, start)
+        elif args.oneline:
+            validating_files(adoc_files, start, output='oneline')
+        elif args.gitlab:
+            validating_files(adoc_files, start, output='gitlab')
+        elif args.links:
+            lcheck_validate(adoc_files)
+    else:
+        #TODO: get rid of possix path
+        separator = "\n\t"
+        logging.error(f'No adoc files detected. The following paths cannot be validated:\n\t{user_input}\n')
+        sys.exit(2)
 
 
 def cli_args() -> argparse.Namespace:
-    parser = argparse.ArgumentParser(prog='enki')
-    subparsers = parser.add_subparsers(dest='command')
+    parser = argparse.ArgumentParser(
+                        prog = 'enki',
+                        description = 'Enki validation',
+                        epilog = 'Raise an issue: https://github.com/Levi-Leah/enki/issues')
 
-    parser_a = subparsers.add_parser("validate", help="perform validation")
-    group_a = parser_a.add_mutually_exclusive_group()
-    group_a.add_argument("--oneline", action="store_true",
+    group = parser.add_mutually_exclusive_group(required=True)
+
+    parser.add_argument('path', nargs='+', type=Path, help='path to files')
+    group.add_argument('-v', '--validate', action="store_true",
+                         help="perform validation")
+    group.add_argument('-o', '--oneline', action="store_true",
                          help="print one validation error per line")
-    group_a.add_argument("--gitlab", action="store_true",
+    group.add_argument('-g', '--gitlab', action="store_true",
                          help="print validation errors in xml format")
-    group_a.add_argument("--links", action="store_true",
-                         help="perform links validation")
-    parser_a.add_argument("path", nargs='+', type=Path, help='path to files')
-
-    if len(sys.argv) == 1:
-        parser.print_help()
-        sys.exit(1)
+    group.add_argument('-l', '--links', action="store_true",
+                         help="find broken links")
 
     args = parser.parse_args()
 
     return args
 
 
-def validate(user_input: list[Path], args: argparse.Namespace) -> None:
-    """
-    Validate files specified on the command line as user_input.
-    """
+def collect_files(user_input: list[Path]):
     files = []
     unsupported_files = []
 
-    start = time.time()
-
     for path in user_input:
-
         str_path = str(path)
-
         if path.is_dir():
             expanded_files = expand_file_paths(path)
             for file in expanded_files:
@@ -76,33 +92,7 @@ def validate(user_input: list[Path], args: argparse.Namespace) -> None:
         else:
             unsupported_files.append(str_path)
 
-    if unsupported_files:
-        separator = "\n\t"
-        logging.error(f'Unsupported file format. The following files cannot be validated:\n\t{separator.join(unsupported_files)}\n')
-        sys.exit(2)
-
-    if files:
-        if args.oneline:
-            validating_files(files, start, output='oneline')
-        elif args.gitlab:
-            validating_files(files, start, output='gitlab')
-        elif args.links:
-            lcheck_path = os.path.dirname(
-                os.path.abspath(__file__)) + '/lcheck.rb'
-
-            master_adocs = []
-            for file in files:
-                if os.path.basename(file) == 'master.adoc':
-                    master_adocs.append(file)
-
-            if master_adocs:
-                os.system(f'ruby {lcheck_path} {master_adocs}')
-            else:
-                logging.error('No master.adoc detected.')
-
-        else:
-            validating_files(files, start)
-
+    return files, unsupported_files
 
 def expand_file_paths(path: Path) -> list[str]:
     """Expand filepaths."""
@@ -116,7 +106,6 @@ def expand_file_paths(path: Path) -> list[str]:
                     expanded_files.append(file)
 
     return expanded_files
-
 
 # Run the program
 if __name__ == '__main__':
